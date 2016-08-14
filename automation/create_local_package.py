dbf4e0ca1c47e77cca623479b65489fb9f640c97
Lee Winder <lee.winder@gmail.com>
2016-08-14 23:59:52 +0100

    Added a couple of automation scripts which prepare the actual distribution package and generate a local package for testing
    Added these scripts to the Travis CI build steps

---
+++ automation/create_local_package.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+# Imports
+import shutil, os
+import subprocess
+import glob
+
+#
+# Gets the main package folder
+#
+def get_package_folder():
+
+    # Get the path to the distribution package
+    package_path = os.getcwd() + '/../release/package/'
+    destination_path = os.getcwd() + '/../release/' 
+    return package_path, destination_path
+
+
+#
+# Removes any existing packages
+#
+def remove_existing_packages(destination_path):
+    
+    # Make sure any local packages are removed
+    for package_file in glob.glob(destination_path + "*.tgz"):
+        os.remove(package_file)
+
+
+#
+# Main entry function
+#
+def main():
+
+    # Get our folder and remove existing packages
+    package_path, destination_path = get_package_folder()
+    remove_existing_packages(destination_path)
+
+    # Build our package
+    os.chdir(package_path)
+    proc = subprocess.Popen(["npm", "pack"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    std_out, std_err = proc.communicate()
+
+    # Output the comment results
+    if (len(std_out) != 0):
+        print "\n*** Output ***"
+        print std_out + "\n"
+
+    if (len(std_err) != 0):
+        print "\n\n*** Error ***"
+        print std_err + "\n"
+
+    # Did we succeed?
+    if (proc.returncode != 0):
+        print "npm pack failed with a return code of " + str(proc.returncode)
+        exit(proc.returncode)
+
+    # Move the file we just created
+    shutil.move(package_path + std_out.strip(), package_path + '../')
+
+    # Done
+    print "Successfully created package file " + std_out.strip()
+    
+
+#
+# Main entry point
+#
+if __name__ == "__main__":
+    main()
---
+++ automation/prepare_distribution_package.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+# Imports
+import shutil, os
+import distutils.dir_util
+
+#
+# Gets the main package folder
+#
+def create_package_folder():
+
+    # Get the path to the distribution package
+    root_path = os.getcwd() + '/../release'
+    if os.path.exists(root_path):
+        shutil.rmtree(root_path)
+    distribution_folder = '/{}/package'.format(root_path)
+    os.makedirs(distribution_folder)
+
+    # Send it back with the root folder
+    return os.getcwd() + '/../', distribution_folder + '/'
+
+
+#
+# Main entry function
+#
+def main():
+
+    # Get our folder
+    root_folder, distribution_folder = create_package_folder()
+
+    # Copy over the root content
+    shutil.copyfile(root_folder + 'LICENSE', distribution_folder + 'LICENSE')
+    shutil.copyfile(root_folder + 'README.md', distribution_folder + 'README.md')
+
+    # Package content
+    shutil.copyfile(root_folder + 'development/package.json', distribution_folder + 'package.json')
+
+    # Copy over all the source files
+    distutils.dir_util.copy_tree(root_folder + 'development/src/ng2-dynamic-dialog', distribution_folder)
+
+    
+
+#
+# Main entry point
+#
+if __name__ == "__main__":
+    main()
