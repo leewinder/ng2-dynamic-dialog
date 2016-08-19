302abc8ff2936aa33d93c0a5bbc8b9eb1880b659
Lee Winder <lee.winder@gmail.com>
2016-08-19 13:54:32 +0100

    prepare package script now cleans and builds the ts project

--- automation/prepare_distribution_package.py
+++ automation/prepare_distribution_package.py
@@ -7,8 +7,57 @@
 
 # Imports
 import shutil, os
+import fnmatch
 import distutils.dir_util
+import cli
 
+#
+# Gets the current root directory of the project
+# 
+def get_project_root():
+
+    # Get the path to our source
+    file_name = os.path.basename(__file__)
+    script_root = os.path.realpath(__file__).replace(file_name, '')
+
+    source_folder = script_root + '..'
+    return source_folder
+
+
+#
+# Finds all files with a specific extension
+#
+def remove_all_files(directory, extension):
+
+    # Delete everything in the source folders
+    for root, dirnames, filenames in os.walk(directory):
+        for filename in fnmatch.filter(filenames, extension):
+            file_path = os.path.join(root, filename)
+            os.remove(file_path)
+
+
+#
+# Removes all the build files so we can do a clean build
+#
+def clean_build_files():
+
+    # Get our path
+    source_folder = get_project_root() + '/development/src'
+
+    remove_all_files(source_folder, '*.js')
+    remove_all_files(source_folder, '*.js.map')
+    remove_all_files(source_folder, '*.d.ts')
+
+#
+# Builds the Typescript project
+#
+def build_project():
+
+    config_root = get_project_root() + '/development/'
+
+    return_code, std_out, std_err = cli.run_command_line(config_root, "tsc", ['-p', 'tsconfig-ci.json'])
+    if (return_code != 0):
+        exit(return_code)
 
 #
 # Gets the main package folder
@@ -30,6 +79,12 @@ def create_package_folder():
 # Main entry function
 #
 def main():
+
+    # Clean up our current build files
+    clean_build_files()
+
+    # Build the project
+    build_project()
 
     # Get our folder
     root_folder, distribution_folder = create_package_folder()
