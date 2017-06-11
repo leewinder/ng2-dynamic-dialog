404a01eef8d8ac1de494e7049632d20805919e7d
Lee Winder <lee.winder@gmail.com>
2017-06-11 20:52:12 +0100

    Feature/automation-update (#6)
    * Updated automation scripts to remove pylint warnings
    * Travis CI now builds feature branches
    * Updated VS Code settings

--- automation/clean_package_artifacts.py
+++ automation/clean_package_artifacts.py
@@ -1,25 +1,21 @@
+""" Deletes any artifacts generated from the package scripts """
 #!/usr/bin/python
 
-#
-# Deletes any artifacts generated from the package scripts
-#
-
 # Imports
-import shutil, os
-import fnmatch
-import distutils.dir_util
+import os
+import shutil
 import cli
-
 
 #
 # Main entry function
 #
 def main():
+    """ Main entry function """
 
     # Get the path to the distribution package
     root_path = cli.get_project_root() + '/release'
     if os.path.exists(root_path):
-        shutil.rmtree(root_path)    
+        shutil.rmtree(root_path)
 
 
 #
--- automation/publish_release_package.py
+++ automation/publish_release_package.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python
+""" Publishes an NMP package using the output generated from 'prepare_distribution_package.py'
 
-#
-# Publishes an NMP package using the output generated from 'prepare_distribution_package.py'
-#
-# Note that this script relies on the use of Travis CI environment variables, meaning
-# this script can only be run as part of a Travis CI build process
-#
-# It also requires npm-cli-login and NPM_USER, NPM_PASS and NPM_EMAIL present in the envvars
-# npm install -g npm-cli-login
-#
+Note that this script relies on the use of Travis CI environment variables, meaning
+this script can only be run as part of a Travis CI build process
+
+It also requires npm-cli-login and NPM_USER, NPM_PASS and NPM_EMAIL present in the envvars
+npm install -g npm-cli-login """
+#!/usr/bin/python
 
 # Imports
 import os
@@ -19,11 +16,12 @@ import cli
 # Gets the main package folder
 #
 def get_package_folder():
+    """ Gets the main package folder """
 
     project_path = cli.get_project_root()
 
     # Get the path to the distribution package
-    package_path = project_path + '/release/package/' 
+    package_path = project_path + '/release/package/'
     return package_path
 
 
@@ -31,6 +29,7 @@ def get_package_folder():
 # Verifies the branch we're on
 #
 def verify_branch_name():
+    """ Verifies the branch we're on """
 
     # Do we have the Environment variable to detect the branch?
     # We only run this script on the master branch
@@ -40,12 +39,19 @@ def verify_branch_name():
     except KeyError:
         print "Unable to access the 'TRAVIS_BRANCH' environment variable\n"
 
-    # We only run on master
-    if (branch_name.lower() != 'master'):
+    # Check it's a push and nothing else
+    event_type = 'unknown'
+    try:
+        event_type = os.environ['TRAVIS_EVENT_TYPE']
+    except KeyError:
+        print "Unable to access the 'TRAVIS_EVENT_TYPE' environment variable\n"
+
+    # We only run on master when it's a push
+    if branch_name.lower() != 'master' or event_type.lower() != 'push':
 
         # Output the message
-        print "Publishing packages can only be carried out on the 'master' branch"
-        print "The branch being built is '" + branch_name + "' so this step will be skipped'" 
+        print "Publishing is only carried out on the 'master' branch when a push occurs"
+        print "A '{}:{}' is running so this step will be skipped".format(branch_name, event_type)
 
         return False
 
@@ -57,29 +63,30 @@ def verify_branch_name():
 # Main entry function
 #
 def main():
+    """ Main entry function """
 
     # Check we're on a branch we can run on
     branch_valid = verify_branch_name()
-    if (branch_valid == False):
+    if branch_valid is False:
         exit(0)
 
     # Get our folder and remove existing packages
     package_path = get_package_folder()
-    
+
     # Log in
-    return_code, std_out, std_err = cli.run_command_line(package_path, "npm-cli-login", None)
-    if (return_code != 0):
+    return_code, _, _ = cli.run_command_line(package_path, "npm-cli-login", None)
+    if return_code != 0:
         exit(return_code)
 
-    
+
     # Publish our package
-    return_code, std_out, std_err = cli.run_command_line(package_path, "npm", ["publish"])
-    if (return_code != 0):
+    return_code, _, _ = cli.run_command_line(package_path, "npm", ["publish"])
+    if return_code != 0:
         exit(return_code)
 
     # Done
     print "Successfully published package file"
-    
+
 
 #
 # Main entry point
